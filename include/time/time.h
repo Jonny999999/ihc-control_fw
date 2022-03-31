d3f43c62f4aaf9801e477aa2aef3be54abc2fc0a
jonny <jonny@wwad.de>
2022-03-31 08:43:44 +0200

    Create 'evaluateSwitch' class in main.cpp
    Created class for debouncing and detecting rising/falling edge of a switch.
    TODO: move this class to separate file / library
    
    Also added time.h / time.c from old avr project

---
+++ include/time/time.h
@@ -0,0 +1,15 @@
+#ifndef TIME_H
+#define TIME_H
+#include <avr/interrupt.h>
+#include <inttypes.h>
+
+#define TIME_TIMER_COUNT_REGISTER TCNT2
+
+
+void time_init(void);
+
+uint32_t time_get(void);
+
+// more recent time first
+uint32_t time_delta(uint32_t a, uint32_t b);
+#endif
